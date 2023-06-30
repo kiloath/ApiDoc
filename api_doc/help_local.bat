82a9c535b6a52e10c9ecc5879d6816c154986e67
peter <peter.hsu@concords.com.tw>
2023-06-30 10:02:20 +0800

    調整

--- api_doc/help_local.bat
+++ api_doc/help_local.bat
@@ -1,8 +1,16 @@
-rmdir /S /Q obj
+@echo off
+rmdir /S /Q _site
+rmdir /S /Q rustdoc
+:: rmdir /S /Q obj
 pushd .
 cd tasks/diary/
 cmd /c tj3 index.tjp
 popd
 cargo doc --manifest-path ../rust/Cargo.toml --target-dir rustdoc
+REM 使用免安裝NET SDK時 /*
+SET DOTNET_ROOT=D:\Portable\dotnet-sdk-8\
+SET PATH=D:\Portable\dotnet-sdk-8;%PATH%
+REM 使用免安裝NET SDK時 */
 docfx docfx_local.json
 start _site\index.html
+PAUSE
--- api_doc/help_server.bat
+++ api_doc/help_server.bat
@@ -1,9 +1,15 @@
-rmdir /S /Q obj
+@echo off
+rmdir /S /Q _site
+rmdir /S /Q rustdoc
+:: rmdir /S /Q obj
 pushd .
 cd tasks/diary/
 cmd /c tj3 index.tjp
 popd
 cargo doc --manifest-path ../rust/Cargo.toml --target-dir rustdoc
+:: SET DOTNET_ROOT=D:\Portable\dotnet-sdk-8\
+:: SET PATH=D:\Portable\dotnet-sdk-8;%PATH%
 cmd /c start docfx docfx_server.json --serve
 timeout /t 5
 start http://localhost:8080
+PAUSE
